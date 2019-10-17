b6faa5b97fa6759f71123bf864f3db60db4acb08
rojo2530 <jcantosg@gmail.com>
2019-10-17 11:13:40 +0200

    Añado componente Navbar

---
+++ wallakeep/src/components/Adverts.js
@@ -0,0 +1,11 @@
+// import Navbar from './Navbar';
+import React from 'react';
+import Navbar from './Navbar';
+
+export default class Adverts extends React.Component {
+  render () {
+    return (
+      <Navbar />
+    )
+  } 
+}
